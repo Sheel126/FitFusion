b4f44969db5f390edb22d30628fb5263893965ec
Sheel Patel <scpatel6@ncsu.edu>
2024-02-03 15:43:39 -0500

    added initial code for main page and navbar

--- fit-fusion-app/src/components/fitnessbox.js
+++ fit-fusion-app/src/components/fitnessbox.js
@@ -1,20 +1,20 @@
 // components/FitnessBox.js
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material';
+import Typography from '@mui/material';
 
 const FitnessBox = ({ totalCalories, calories }) => {
   // Define the size based on calories. This is a basic example, adjust the formula as needed.
-  const size = totalCalories /calories;
+  // const size = totalCalories / calories;
 
   return (
     <Box sx={{
         height: totalCalories,
-        width: 
+        width: 15
     }}>
         <Box
             sx={{
-                width: 15%,
-                height: size,
+                width: 15,
+                height: calories,
                 bgcolor: 'background.paper',
                 border: 1,
                 borderColor: 'primary.main',
